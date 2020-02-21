2b5cc26afabf2f40788053e192fc694ebd68d153
marciocardozodev <tec.marciocardozo@gmail.com>
2020-02-20 22:03:24 -0300

    Subindo projeto ASP.NET MVC

---
+++ ProjetoTesteMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjetoTesteMVC.Startup))]
+namespace ProjetoTesteMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
